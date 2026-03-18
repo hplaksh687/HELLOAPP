7c1cbc7f6684a2b786335f288061a1f96cf71979
Laksh H P <lakshhp@Lakshs-MacBook-Air-306.local>
2026-03-18 13:36:09 +0530

    UC2: display name using command-line argument

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,22 +2,8 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Default case
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
-        }
+        String name = (args.length > 0) ? args[0] : "World";
 
-        // Build names using enhanced for loop
-        StringBuilder sb = new StringBuilder();
-
-        for (String name : args) {
-            sb.append(name).append(", ");
-        }
-
-        // Remove last comma and space
-        sb.setLength(sb.length() - 2);
-
-        System.out.println("Hello, " + sb.toString() + "!");
+        System.out.println("Hello, " + name + "!");
     }
 }
