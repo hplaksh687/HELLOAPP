161c24de04dd7ccb147f5c06e6cf79e58d88e58b
Laksh H P <lakshhp@Lakshs-MacBook-Air-306.local>
2026-03-18 13:32:24 +0530

    UC5: display multiple names using enhanced for loop

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,10 +2,22 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        String names = (args.length > 0)
-                ? String.join(", ", args)
-                : "World";
+        // Default case
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
 
-        System.out.println("Hello, " + names + "!");
+        // Build names using enhanced for loop
+        StringBuilder sb = new StringBuilder();
+
+        for (String name : args) {
+            sb.append(name).append(", ");
+        }
+
+        // Remove last comma and space
+        sb.setLength(sb.length() - 2);
+
+        System.out.println("Hello, " + sb.toString() + "!");
     }
 }
